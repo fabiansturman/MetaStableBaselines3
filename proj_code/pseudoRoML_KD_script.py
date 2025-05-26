e6351e3cebdda12919365c4a8af74e2f44361ec3
fabiansturman <60733643+fabiansturman@users.noreply.github.com>
2025-05-26 19:46:41 +0100

    Update pseudoRoML_KD_script.py

--- proj_code/pseudoRoML_KD_script.py
+++ proj_code/pseudoRoML_KD_script.py
@@ -21,7 +21,7 @@ import matplotlib.pyplot as plt
 device = 'cpu'# 'cuda' #  #doing cpu as A2C with MlpPolicy (rather than CNNpolicy) in stablebaseline is faster on CPU, and the meta gradinet beign faster on GPU (even if it is) is not *that* much faster - it is about two(ish) times slower overall based on one run each with two meta iterations, so better on cpu in this case
 torch.set_default_device(device)
 #######################################################
-model_save_path = "saved_models/25May_TestingPseudoRoML_A2C_3a" #simulatenously testing my new maml syntax out and doing PPO (if somethings up, then try A2C with my new maml syntax to see if its my PPO that is wrong or the MAML syntax (too?))
+model_save_path = "saved_models/26May_TestingPseudoRoML_A2C_3" #simulatenously testing my new maml syntax out and doing PPO (if somethings up, then try A2C with my new maml syntax to see if its my PPO that is wrong or the MAML syntax (too?))
 print(model_save_path)
 import os
 os.mkdir(model_save_path)
@@ -42,11 +42,11 @@ if device != 'cpu':
 #Hyperparameters
 adapt_lr =  7e-4
 meta_lr = 0.0005 
-meta_iterations = 1000#500#1250
+meta_iterations = 2000#500#1250
 adapt_timesteps = 32*4 #for this enviornment, each episode is exactly 32 timesteps, so multiple of 32 means full number of eps experienced for each task
 eval_timesteps = 100
 tasks_per_loop = 40#60
-adapt_visualisations = 30
+adapt_visualisations = 50
 M=1
 
 vis_timesteps = meta_iterations//adapt_visualisations #denominator is number of visualisations we want
@@ -59,23 +59,22 @@ env = gym.make("KhazadDum-v1") # can access wrapped env with "env.unwrapped" (e.
 env.unwrapped.exp_bonus = 1; env.unwrapped.bridge_bonus_factor = 2 #this should incentivise getting to the target asap, and incentivise going onto the bridge
 
 #Pseudo-Roml - Add an offset to the sampled noise (for alpha-cvar, it is -mean* ln(alpha) by memoryless property off exponentials)
-env.unwrapped.noise_offset = -1*env.unwrapped.average_noise*np.log(0.025) #turned to 0.025 rather than 0.05
+env.unwrapped.noise_offset = -1*env.unwrapped.average_noise*np.log(0.025) 
 
 #Make meta-policy and meta-optimiser
-meta_agent = A2C("MlpPolicy", env, verbose=0, learning_rate=adapt_lr, device=device, ent_coef=0.25,
+meta_agent = A2C("MlpPolicy", env, verbose=0, learning_rate=adapt_lr, device=device, ent_coef=0.05,
                  meta_learning=True, M=M, adapt_timesteps=adapt_timesteps, eval_timesteps=eval_timesteps) #we train the meta_agent to do well at adapting to new envs (i.e. meta learning) in our env distribution
 meta_opt = optim.Adam(meta_agent.policy.parameters(), lr=meta_lr)
 
+
+#######################################################
 #Logging variables
 meta_losses = []
 meta_rets = []
-best_meta_ret = None
-best_meta_ret_it = -1
-#######################################################
-print("Loading in previous training, doing an extra 1000 its to get it up to 2000 its worth of training")
-meta_agent.policy.load_state_dict(torch.load("saved_models/25May_TestingPseudoRoML_A2C_3/final", weights_only=True)) 
-
-#######################################################
+best_return_past_400 = -100
+best_return_it_past_400 = -1
+best_loss_past_400 = -100
+best_loss_it_past_400 = -1
 
 #Outer meta-learning loop
 for meta_it in tqdm(range(meta_iterations)):
@@ -95,13 +94,18 @@ for meta_it in tqdm(range(meta_iterations)):
     meta_loss.backward()
     meta_opt.step()
 
-    #Save(/override) the best performing model (w.r.t performance against adaptation task set)
-    if best_meta_ret is None or meta_ret>best_meta_ret :
-        best_meta_ret = meta_ret
-        best_meta_ret_it = meta_it
-        torch.save(meta_agent.policy.state_dict(), f"{model_save_path}/best_val_meta_ret") #OK to just save {model.policy} as that is all being meta-optimised
-            #TODO: could it be that the highest return happens before the end of training because actually it then optimises loss and that gets us to the target in a way that return doesnt capture? not sure that makes sense so probs not....??? unlike loss being wierd before the critic is trained (I think thta is the source of the wierdness) return should always tell us whats good and what isnt rihgt?
+    #Save(/override) the best performing model(s) (w.r.t performance against adaptation task set)
+    if meta_it >=400 and meta_ret>=best_return_past_400: #ret and loss starts pretty good as the model is not trained but the actual performance is not all that so only do this after a little burnin
+                #We do>= rather than > to bias towards more recent models, assuming extra stuff has been learnt since that maybe isnt capotured by the metric we are using 
+        best_return_past_400 = meta_ret
+        best_return_it_past_400 = meta_it
+        torch.save(meta_agent.policy.state_dict(), f"{model_save_path}/best_val_ret_past400metaiterations")
+    if meta_it >=400 and meta_loss<=best_loss_past_400: #ret and loss starts pretty good as the model is not trained but the actual performance is not all that so only do this after a little burnin
+        best_loss_past_400 = meta_loss
+        best_loss_it_past_400 = meta_it
+        torch.save(meta_agent.policy.state_dict(), f"{model_save_path}/best_val_loss_past400metaiterations")
 
+    
     #Track meta_training curve
     meta_losses.append(meta_loss.detach().item())
     meta_rets.append(meta_ret.item())
@@ -149,7 +153,7 @@ loaded_meta_agent = A2C("MlpPolicy", env, verbose=0, learning_rate=adapt_lr, dev
                  meta_learning=True, M=M, adapt_timesteps=adapt_timesteps, eval_timesteps=eval_timesteps)
 loaded_meta_agent.policy.load_state_dict(torch.load(f"{model_save_path}/final", weights_only=True)) 
     #can also load in intermediate saved envs from training
-#######################################################
+
 print("Qualitative evaluation of loaded model")
 #Seeing it manage in a range of environments - this one is taken from loading the best model from a 400 meta-step training, which clearly isnt quite all that 
 dim = 3
@@ -174,8 +178,67 @@ plt.tight_layout()
 plt.savefig(f"{model_save_path}/finalQualititativeEvaluation")
 plt.clf()
 #######################################################
+print("Loading in model")
+loaded_meta_agent = A2C("MlpPolicy", env, verbose=0, learning_rate=adapt_lr, device=device,
+                 meta_learning=True, M=M, adapt_timesteps=adapt_timesteps, eval_timesteps=eval_timesteps)
+loaded_meta_agent.policy.load_state_dict(torch.load(f"{model_save_path}/best_val_ret_past400metaiterations", weights_only=True))
+
+
+print("Qualitative evaluation of loaded model")
+#Seeing it manage in a range of environments - this one is taken from loading the best model from a 400 meta-step training, which clearly isnt quite all that 
+dim = 3
+fig, axs = plt.subplots(dim, dim, figsize=(10, 8))
+
+for t in tqdm(range(dim*dim)):
+    #Perform few shot adaption to environment
+    env.unwrapped.reset_task() #randomly selects task from environment to reset it to
+    _,_,adapted_policy = loaded_meta_agent.meta_adapt(task = env.unwrapped.get_task(), M=1)
+    adapted_policy = adapted_policy[0] # we set M=1 above so only 1 policy adapted here
+
+    #Test against a new trajectory from that state (else we are showing something it trained to and before the final training step)
+    loaded_meta_agent.evaluate_policy(total_timesteps=32, policy=adapted_policy)
+
+    #Plot this run
+    x = t//dim
+    y = t%dim
+    axs[x,y] = env.unwrapped.show_state(axs[x,y],show_task=True, text_coords=(2,1))    
+    axs[x,y].set_axis_off()
+
+plt.tight_layout()
+plt.savefig(f"{model_save_path}/finalQualititativeEvaluation_forBestRetPast400Its")
+plt.clf()
 
 #######################################################
+
+print("Loading in model")
+loaded_meta_agent = A2C("MlpPolicy", env, verbose=0, learning_rate=adapt_lr, device=device,
+                 meta_learning=True, M=M, adapt_timesteps=adapt_timesteps, eval_timesteps=eval_timesteps)
+loaded_meta_agent.policy.load_state_dict(torch.load(f"{model_save_path}/best_val_loss_past400metaiterations", weights_only=True))
+
+
+print("Qualitative evaluation of loaded model")
+#Seeing it manage in a range of environments - this one is taken from loading the best model from a 400 meta-step training, which clearly isnt quite all that 
+dim = 3
+fig, axs = plt.subplots(dim, dim, figsize=(10, 8))
+
+for t in tqdm(range(dim*dim)):
+    #Perform few shot adaption to environment
+    env.unwrapped.reset_task() #randomly selects task from environment to reset it to
+    _,_,adapted_policy = loaded_meta_agent.meta_adapt(task = env.unwrapped.get_task(), M=1)
+    adapted_policy = adapted_policy[0] # we set M=1 above so only 1 policy adapted here
+
+    #Test against a new trajectory from that state (else we are showing something it trained to and before the final training step)
+    loaded_meta_agent.evaluate_policy(total_timesteps=32, policy=adapted_policy)
+
+    #Plot this run
+    x = t//dim
+    y = t%dim
+    axs[x,y] = env.unwrapped.show_state(axs[x,y],show_task=True, text_coords=(2,1))    
+    axs[x,y].set_axis_off()
+
+plt.tight_layout()
+plt.savefig(f"{model_save_path}/finalQualititativeEvaluation_forBestLossPast400Its")
+plt.clf()
 
 #######################################################
 
