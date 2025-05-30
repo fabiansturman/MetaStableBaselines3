c3f8dc4bb5f9dd584971a0f048284ddb2d1ba241
fabiansturman <60733643+fabiansturman@users.noreply.github.com>
2025-05-30 09:47:26 +0100

    Update pseudoRoML_KD_script4.py

--- proj_code/pseudoRoML_KD_script4.py
+++ proj_code/pseudoRoML_KD_script4.py
@@ -21,7 +21,7 @@ import matplotlib.pyplot as plt
 device = 'cpu'# 'cuda' #  #doing cpu as A2C with MlpPolicy (rather than CNNpolicy) in stablebaseline is faster on CPU, and the meta gradinet beign faster on GPU (even if it is) is not *that* much faster - it is about two(ish) times slower overall based on one run each with two meta iterations, so better on cpu in this case
 torch.set_default_device(device)
 #######################################################
-model_save_path = "saved_models/26May_TestingPseudoRoML_A2C_3a_MAML" #simulatenously testing my new maml syntax out and doing PPO (if somethings up, then try A2C with my new maml syntax to see if its my PPO that is wrong or the MAML syntax (too?))
+model_save_path = "saved_models/27May_MAML_A2C_KDcont3_ep64" #simulatenously testing my new maml syntax out and doing PPO (if somethings up, then try A2C with my new maml syntax to see if its my PPO that is wrong or the MAML syntax (too?))
 print(model_save_path)
 import os
 os.mkdir(model_save_path)
@@ -42,7 +42,7 @@ if device != 'cpu':
 #Hyperparameters
 adapt_lr =  7e-4
 meta_lr = 0.0005 
-meta_iterations = 2000#500#1250
+meta_iterations = 1000#500#1250
 adapt_timesteps = 32*4 #for this enviornment, each episode is exactly 32 timesteps, so multiple of 32 means full number of eps experienced for each task
 eval_timesteps = 100
 tasks_per_loop = 40#60
@@ -55,14 +55,14 @@ if vis_timesteps == 0:
 
 #Make meta-environment
 import fabian.envs.khazad_dum_gymn 
-env = gym.make("KhazadDum-v1") # can access wrapped env with "env.unwrapped" (e.g. to reset task)
+env = gym.make("KhazadDum-v1", continuous=True, max_speed=0.5, max_episode_steps=64) # can access wrapped env with "env.unwrapped" (e.g. to reset task)
 env.unwrapped.exp_bonus = 1; env.unwrapped.bridge_bonus_factor = 2 #this should incentivise getting to the target asap, and incentivise going onto the bridge
 
 #Pseudo-Roml - Add an offset to the sampled noise (for alpha-cvar, it is -mean* ln(alpha) by memoryless property off exponentials)
 #env.unwrapped.noise_offset = -1*env.unwrapped.average_noise*np.log(0.025) 
 
 #Make meta-policy and meta-optimiser
-meta_agent = A2C("MlpPolicy", env, verbose=0, learning_rate=adapt_lr, device=device, ent_coef=0.01,
+meta_agent = A2C("MlpPolicy", env, verbose=0, learning_rate=adapt_lr, device=device,
                  meta_learning=True, M=M, adapt_timesteps=adapt_timesteps, eval_timesteps=eval_timesteps) #we train the meta_agent to do well at adapting to new envs (i.e. meta learning) in our env distribution
 meta_opt = optim.Adam(meta_agent.policy.parameters(), lr=meta_lr)
 
