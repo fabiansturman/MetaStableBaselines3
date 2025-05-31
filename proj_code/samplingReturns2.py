c1afd8fb9b7cfc030ca076e4fbf3e3e56b576037
fabiansturman <60733643+fabiansturman@users.noreply.github.com>
2025-05-31 21:56:05 +0100

    sampling returns it 900

--- proj_code/samplingReturns2.py
+++ proj_code/samplingReturns2.py
@@ -39,7 +39,7 @@ env.unwrapped.exp_bonus = 1; env.unwrapped.bridge_bonus_factor = 2 #this should 
 
 
 
-its = [800]#[0,100,200,300]#,800,900]
+its = [900]#[0,100,200,300]#,800,900]
 
 for it in its:
     print(f"Getting return distribution for model at meta iteration {it}")
